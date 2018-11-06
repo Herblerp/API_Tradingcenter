335010114fd93f0dda701945f10ed96ba2c88195
Quentin Vekeman-Marcelis <quentin.vekeman-marcelis@student.ehb.be>
2018-11-06 19:55:27 +0100

    Add migrations

---
+++ Migrations/20181106115645_skjh.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_Tradingcenter.Migrations
+{
+    public partial class skjh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastRefresh",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Exchange",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastRefresh",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Symbol",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Exchange",
+                table: "Orders",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Currency",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
